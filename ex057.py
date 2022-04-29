55e4d718bb94aba3d003355a15e98beb31d9358f
BielNovais <101150531+BielNovais@users.noreply.github.com>
2022-04-29 01:24:41 -0300

    Delete ex057.py

--- ex057.py
+++
@@ -1,4 +0,0 @@
-sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
-while sexo not in 'MmFf':
-    sexo = str(input('Dados invalidos. Por favor, informe seu sexo: ')).strip().upper()[0]
-print('Sexo {} regitrado com sucesso'.format(sexo))
